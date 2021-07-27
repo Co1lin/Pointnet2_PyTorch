145babff139529d92a159a1156ae181049b3fc06
air <air@air.cloud>
2021-07-27 14:30:33 +0800

    add missing functions required by RfDNet

--- pointnet2_ops_lib/pointnet2_ops/pointnet2_modules.py
+++ pointnet2_ops_lib/pointnet2_ops/pointnet2_modules.py
@@ -207,3 +207,248 @@ class PointnetFPModule(nn.Module):
         new_features = self.mlp(new_features)
 
         return new_features.squeeze(-1)
+
+class PointnetSAModuleVotes(nn.Module):
+    ''' Modified based on _PointnetSAModuleBase and PointnetSAModuleMSG
+    with extra support for returning point indices for getting their GT votes '''
+
+    def __init__(
+            self,
+            *,
+            mlp: List[int],
+            npoint: int = None,
+            radius: float = None,
+            nsample: int = None,
+            bn: bool = True,
+            use_xyz: bool = True,
+            pooling: str = 'max',
+            sigma: float = None,  # for RBF pooling
+            normalize_xyz: bool = False,  # noramlize local XYZ with radius
+            sample_uniformly: bool = False,
+            ret_unique_cnt: bool = False
+    ):
+        super().__init__()
+
+        self.npoint = npoint
+        self.radius = radius
+        self.nsample = nsample
+        self.pooling = pooling
+        self.mlp_module = None
+        self.use_xyz = use_xyz
+        self.sigma = sigma
+        if self.sigma is None:
+            self.sigma = self.radius / 2
+        self.normalize_xyz = normalize_xyz
+        self.ret_unique_cnt = ret_unique_cnt
+
+        if npoint is not None:
+            self.grouper = pointnet2_utils.QueryAndGroup(radius, nsample,
+                                                         use_xyz=use_xyz, ret_grouped_xyz=True,
+                                                         normalize_xyz=normalize_xyz,
+                                                         sample_uniformly=sample_uniformly,
+                                                         ret_unique_cnt=ret_unique_cnt)
+        else:
+            self.grouper = pointnet2_utils.GroupAll(use_xyz, ret_grouped_xyz=True)
+
+        mlp_spec = mlp
+        if use_xyz and len(mlp_spec) > 0:
+            mlp_spec[0] += 3
+        self.mlp_module = build_shared_mlp(mlp_spec, bn=bn)
+
+    def forward(self, xyz: torch.Tensor,
+                features: torch.Tensor = None,
+                inds: torch.Tensor = None):# -> (torch.Tensor, torch.Tensor):
+        r"""
+        Parameters
+        ----------
+        xyz : torch.Tensor
+            (B, N, 3) tensor of the xyz coordinates of the features
+        features : torch.Tensor
+            (B, C, N) tensor of the descriptors of the the features
+        inds : torch.Tensor
+            (B, npoint) tensor that stores index to the xyz points (values in 0-N-1)
+
+        Returns
+        -------
+        new_xyz : torch.Tensor
+            (B, npoint, 3) tensor of the new features' xyz
+        new_features : torch.Tensor
+            (B, \sum_k(mlps[k][-1]), npoint) tensor of the new_features descriptors
+        inds: torch.Tensor
+            (B, npoint) tensor of the inds
+        """
+
+        xyz_flipped = xyz.transpose(1, 2).contiguous()
+        if inds is None:
+            inds = pointnet2_utils.furthest_point_sample(xyz, self.npoint) if self.npoint is not None else None
+        else:
+            assert (inds.shape[1] == self.npoint)
+        new_xyz = pointnet2_utils.gather_operation(
+            xyz_flipped, inds
+        ).transpose(1, 2).contiguous() if self.npoint is not None else None
+
+        if not self.ret_unique_cnt:
+            grouped_features, grouped_xyz = self.grouper(
+                xyz, new_xyz, features
+            )  # (B, C, npoint, nsample)
+        else:
+            grouped_features, grouped_xyz, unique_cnt = self.grouper(
+                xyz, new_xyz, features
+            )  # (B, C, npoint, nsample), (B,3,npoint,nsample), (B,npoint)
+
+        new_features = self.mlp_module(
+            grouped_features
+        )  # (B, mlp[-1], npoint, nsample)
+        if self.pooling == 'max':
+            new_features = F.max_pool2d(
+                new_features, kernel_size=[1, new_features.size(3)]
+            )  # (B, mlp[-1], npoint, 1)
+        elif self.pooling == 'avg':
+            new_features = F.avg_pool2d(
+                new_features, kernel_size=[1, new_features.size(3)]
+            )  # (B, mlp[-1], npoint, 1)
+        elif self.pooling == 'rbf':
+            # Use radial basis function kernel for weighted sum of features (normalized by nsample and sigma)
+            # Ref: https://en.wikipedia.org/wiki/Radial_basis_function_kernel
+            rbf = torch.exp(
+                -1 * grouped_xyz.pow(2).sum(1, keepdim=False) / (self.sigma ** 2) / 2)  # (B, npoint, nsample)
+            new_features = torch.sum(new_features * rbf.unsqueeze(1), -1, keepdim=True) / float(
+                self.nsample)  # (B, mlp[-1], npoint, 1)
+        new_features = new_features.squeeze(-1)  # (B, mlp[-1], npoint)
+
+        if not self.ret_unique_cnt:
+            return new_xyz, new_features, inds
+        else:
+            return new_xyz, new_features, inds, unique_cnt
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        if hasattr(m, 'weight') and hasattr(m.weight, 'data'):
+            torch.nn.init.constant_(m.weight.data, 0.0)
+        if hasattr(m, 'bias') and hasattr(m.bias, 'data'):
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find('Linear') != -1:
+        if hasattr(m, 'weight') and hasattr(m.weight, 'data'):
+            torch.nn.init.constant_(m.weight.data, 0.0)
+        if hasattr(m, 'bias') and hasattr(m.bias, 'data'):
+            torch.nn.init.constant_(m.bias.data, 0.0)
+
+class STN3d(nn.Module):
+    def __init__(self, num_points=2500):
+        super(STN3d, self).__init__()
+        self.num_points = num_points
+        self.conv1 = nn.Conv1d(3, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 256, 1)
+        self.mp1 = nn.MaxPool1d(num_points)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 12)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(256)
+        self.bn4 = nn.BatchNorm1d(128)
+        self.bn5 = nn.BatchNorm1d(64)
+
+        self.apply(weights_init)
+
+    def forward(self, grouped_xyz):
+        device = grouped_xyz.device
+        batch_size, _, N_proposals, _ = grouped_xyz.size()
+        grouped_xyz = grouped_xyz.transpose(2, 1).contiguous().view(batch_size * N_proposals, 3, self.num_points)
+
+        x = self.relu(self.bn1(self.conv1(grouped_xyz)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
+        x = self.mp1(x)
+        x = x.squeeze(2)
+
+        x = self.relu(self.bn4(self.fc1(x)))
+        x = self.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = torch.tensor([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]]).float().view(1, 12).to(device)
+
+        x = x + iden
+        x = x.view(batch_size * N_proposals, 3, 4)
+
+        # coordinates tranformation
+        grouped_xyz = torch.bmm(x[:, :, :3], grouped_xyz) + x[:, :, 3].unsqueeze(-1)
+        grouped_xyz = grouped_xyz.view(batch_size, N_proposals, 3, -1)
+
+        return grouped_xyz.transpose(1, 2)
+
+
+class STN_Group(nn.Module):
+
+    def __init__(
+            self,
+            radius: float = None,
+            nsample: int = None,
+            use_xyz: bool = True,
+            normalize_xyz: bool = False,  # noramlize local XYZ with radius
+            sample_uniformly: bool = False,
+            ret_unique_cnt: bool = False
+    ):
+        super().__init__()
+
+        self.radius = radius
+        self.nsample = nsample
+        self.use_xyz = use_xyz
+        self.normalize_xyz = normalize_xyz
+        self.ret_unique_cnt = ret_unique_cnt
+
+        self.grouper = pointnet2_utils.QueryAndGroup(radius, nsample,
+                                                     use_xyz=use_xyz, ret_grouped_xyz=True, normalize_xyz=normalize_xyz,
+                                                     sample_uniformly=sample_uniformly, ret_unique_cnt=ret_unique_cnt)
+
+        self.stn3d = STN3d(num_points=nsample)
+
+    def forward(self, xyz: torch.Tensor,
+                features: torch.Tensor = None,
+                new_xyz: torch.Tensor = None,
+                orientations: torch.Tensor = None) -> (torch.Tensor, torch.Tensor):
+        r"""
+        Parameters
+        ----------
+        xyz : torch.Tensor
+            (B, N, 3) tensor of the xyz coordinates of the features
+        features : torch.Tensor
+            (B, C, N) tensor of the descriptors of the the features
+        new_xyz : torch.Tensor
+            (B, npoint, 3) tensor of the coordinates to be grouped at
+        """
+        if not self.ret_unique_cnt:
+            grouped_features, grouped_xyz = self.grouper(
+                xyz, new_xyz, features
+            )  # (B, C, npoint, nsample)
+        else:
+            grouped_features, grouped_xyz, unique_cnt = self.grouper(
+                xyz, new_xyz, features
+            )  # (B, C, npoint, nsample), (B,3,npoint,nsample), (B,npoint)
+
+        # align objects to the canonical system.
+        rot_matrix = torch.zeros(size=[*orientations.size(), 3, 3]).to(orientations.device)
+        rot_matrix[..., 0, 0] = torch.cos(orientations)
+        rot_matrix[..., 0, 1] = torch.sin(orientations)
+        rot_matrix[..., 1, 1] = torch.cos(orientations)
+        rot_matrix[..., 1, 0] = -torch.sin(orientations)
+        rot_matrix[..., 2, 2] = 1.
+
+        batch_size, N_proposals = orientations.size()
+
+        grouped_xyz = torch.bmm(rot_matrix.view(batch_size * N_proposals, 3, 3),
+                                grouped_xyz.transpose(1, 2).contiguous().view(batch_size * N_proposals, 3, -1))
+
+        grouped_xyz = grouped_xyz.view(batch_size, N_proposals, 3, -1).transpose(1, 2).contiguous()
+
+        # Involve STN to learn a spatial transformation (3 X 4 matrix)
+        grouped_xyz = self.stn3d(grouped_xyz)
+
+        if not self.ret_unique_cnt:
+            return grouped_xyz, grouped_features
+        else:
+            return grouped_xyz, grouped_features, unique_cnt
